cdbb8c3d49dca679de1865ea8adc4f4bd2517a97
aran8276 <aran8276@gmail.com>
2026-04-06 20:34:55 +0700

    feat: fallzone

--- steve.gd
+++ steve.gd
@@ -57,3 +57,7 @@ func align_with_floor(floor_normal: Vector3) -> void:
 	xform.basis.y = floor_normal
 	xform.basis.x = - xform.basis.z.cross(floor_normal)
 	xform.basis = xform.basis.orthonormalized()
+
+
+func _on_fall_zone_body_entered(_body: Node3D) -> void:
+	get_tree().reload_current_scene()
